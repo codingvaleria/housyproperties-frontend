99fc943adc4e2c558cd6e207852be0bfe39632e2
DuncanLangat6515 <108465603+DuncanLangat6515@users.noreply.github.com>
2023-01-11 22:07:55 +0300

    Feat/about (#11)

--- src/tenant/components/contact/Contact.js
+++ src/tenant/components/contact/Contact.js
@@ -1,7 +1,40 @@
 import React from "react";
+import "./Contact.css"
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+    <div id="overlay">
+                      <h2>Contact Us</h2>
+                <form id="form_card"> 
+                    
+                    <div>
+                            <input type="text" 
+                            placeholder="Your Name"
+                            id="name"
+                            />
+                            <input type="text" 
+                            placeholder="Your Email"
+                            id="email"
+                            />
+                            <input type="Number" 
+                            placeholder="Phone No:"   
+                            minLength={10}
+                            id="phone"
+                            />
+                    </div>
+                    <div id="textarea_part">
+                            <textarea  
+                            placeholder="  Type Message"  
+                            type="text" 
+                            id="message" />
+                            <button id="btn">Send Message</button>
+                   </div>
+                    
+                </form>
+            </div>
+          </>
+  );
 }
 
 export default Contact;
