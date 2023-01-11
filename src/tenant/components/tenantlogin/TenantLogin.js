f45d27be0b520383b99ab4a887bfce61b37da621
Ashley <ashleynjeri7@gmail.com>
2023-01-11 17:17:24 +0300

    correction

--- src/tenant/components/tenantlogin/TenantLogin.js
+++ src/tenant/components/tenantlogin/TenantLogin.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import logo from "../logo.svg";
-import "./TenantLogin.css";
+
 
 const TenantLogin = ({ onLogin }) => {
   const [email, setemail] = useState("");
