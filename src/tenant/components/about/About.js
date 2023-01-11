19603969a37751948a53249ac23be83459140157
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2023-01-11 20:58:21 +0300

    add functionality and render it

--- src/tenant/components/about/About.js
+++ src/tenant/components/about/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
-
+import Contact from "../contact/Contact";
 import "./About.css"
 import Image from "../../../images/apa.png";
 import Pic from "../../../images/apart2.jpg";
@@ -19,6 +19,7 @@ function About() {
                 <div> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br></br>Ut enim ad minim veniam, quis nostrud exercitation ullamco<br></br> laboris nisi ut aliquip ex ea commodo consequat. <br></br>Duis aute irure dolor in reprehenderit in voluptate velit<br></br> esse cillum dolore eu fugiat nulla pariatur. <br></br>Excepteur sint occaecat cupidatat non proident, <br></br>sunt in culpa qui officia deserunt mollit anim id est laborum</p></div>
                 <div><img src={Pic} alt="apart2"/></div>
             </div>
+            <Contact/>
           
         <Footer/>
     </div>
