3266dadeb0bf4a20e764c065ac83ac215521af8b
Ashley <ashleynjeri7@gmail.com>
2023-01-11 16:56:08 +0300

    function component

--- src/tenant/components/tenantlogin/TenantLogin.js
+++ src/tenant/components/tenantlogin/TenantLogin.js
@@ -1,7 +1,33 @@
 import React from "react";
 
-function TenantLogin() {
-  return <div>TenantLogin</div>;
-}
+const TenantLogin = ({ onLogin }) => {
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e) => { 
+      e.preventDefault();
+      setIsLoading(true);
+      fetch("http://localhost:3000/login", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+              email,
+              password,
+          }).then((r) => {
+              if (r.ok) {
+                  r.json().then((user) => onLogin(user));
+              } else {
+                  r.json().then((err) => setErrors(err.errors));
+              }
+          }
+          ),
+      });
+  };
+
+ 
 
 export default TenantLogin;
