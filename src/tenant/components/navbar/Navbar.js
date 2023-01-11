0b8a5148cd1bf4e2ef6c30cd4f9a8ee0c8705dc5
codingvaleria <codingvaleria@gmail.com>
2023-01-11 13:11:29 +0300

    Add navbar component and styling

--- src/tenant/components/navbar/Navbar.js
+++ src/tenant/components/navbar/Navbar.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
+// import logo from "../../tenant/images/logo.png";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+  return (
+    <div className="navbar">
+      <span className="nav-logo">{/* <img src={logo} alt="logo" /> */}</span>
+      <button className="hamburger-menu" onClick={handleShowNavbar}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="open-icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          />
+        </svg>
+      </button>
+      <div className={showNavbar ? "nav-items" : "nav-items hidden"}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact-us">Contact us</NavLink>
+        <NavLink to="/sign-in">Sign in</NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
