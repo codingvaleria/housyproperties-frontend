d13460d27ea71e38dee4b1f9da4bc9bd331e07a3
codingvaleria <106528361+codingvaleria@users.noreply.github.com>
2023-01-11 17:45:38 +0300

    Add adminhome components (#5)

--- src/admin/components/adminhome/AdminBanner.js
+++ src/admin/components/adminhome/AdminBanner.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./AdminBanner.css";
+import apartments from "../../../images/apartments.png";
+import family from "../../../images/family.png";
+import villa from "../../../images/villa.png";
 
-function AdminBanner() {
-  return <div>AdminBanner</div>;
+export default function AdminBanner() {
+  return (
+    <React.Fragment>
+      <div className="property-container">
+        <div className="property-title">
+          <h2> Property Types</h2>
+          <p> Select property to view all actions</p>
+        </div>
+        <div className="property-elements">
+          <img src={family} />
+          <img src={apartments} />
+          <img src={villa} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
-
-export default AdminBanner;
