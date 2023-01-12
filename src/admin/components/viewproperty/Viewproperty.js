c6d38a4cf98fd93b83f5667c0f96adf840c47248
Ashley <ashleynjeri7@gmail.com>
2023-01-12 12:11:51 +0300

    function component added

--- src/admin/components/viewproperty/Viewproperty.js
+++ src/admin/components/viewproperty/Viewproperty.js
@@ -2,7 +2,21 @@ import React from "react";
 import AdminBanner from "../adminhome/AdminBanner";
 import AdminNavBar from "../adminhome/AdminNavbar";
 
-function Viewproperty() {
+const Viewproperty = () => {
+  const [data, setData] = useState(propertiesData);
+
+  const handleDelete = (index) => {
+    const updatedData = [...data];
+    updatedData.splice(index, 1);
+    setData(updatedData);
+  }
+
+  const handleEdit = (index) => {
+    const updatedData = [...data];
+    updatedData[index].name = "Edited";
+    // make changes to the specific item in updatedData 
+    setData(updatedData);
+}
   return (
     <div>
       <AdminNavBar />
