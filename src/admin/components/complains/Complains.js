9be39defd895d4f9ef1d2a6c834773a0d66956d5
codingvaleria <106528361+codingvaleria@users.noreply.github.com>
2023-01-12 06:23:57 +0300

    Merge #8-#14 to main (#17)

--- src/admin/components/complains/Complains.js
+++ src/admin/components/complains/Complains.js
@@ -1,11 +1,79 @@
 import React from "react";
 import AdminNavBar from "../adminhome/AdminNavbar";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Complains() {
   return (
     <div>
       <AdminNavBar />
-      Complains
+      <div className='tittle'><h2> House Review/complains</h2></div>
+            <table>
+                <th>Name</th>
+                <th>House No</th>
+                <th>Phone No</th>
+                <th>House Type</th>
+                <th>Unit Type</th>
+                <th>location</th>
+                <th>complain</th>
+                <th>Action</th>
+                <tr>
+                    <td>James</td>
+                    <td>12</td>
+                    <td>254712345678</td>
+                    <td>Apartment</td>
+                    <td>2 bdrm</td>
+                    <td>Rongai</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, similique quisquam molestias optio dicta esse? Voluptate magni voluptatem iste excepturi.</td>
+                    <td className='delete'><DeleteIcon/></td>
+                </tr>
+                <tr>
+                    <td>David</td>
+                    <td>12</td>
+                    <td>254712345678</td>
+                    <td>Apartment</td>
+                    <td>2 bdrm</td>
+                    <td>Nairobi</td>
+                    <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis non aliquid aspernatur consequatur voluptates aut doloremque eum deleniti hic totam?</td>
+                    <td className='delete'><DeleteIcon/></td>
+                </tr><tr>
+                    <td>Michael</td>
+                    <td>12</td>
+                    <td>254712345678</td>
+                    <td>Villa</td>
+                    <td>3 bdrm</td>
+                    <td>Nairobi</td>
+                    <td>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam ad asperiores, reprehenderit commodi dolorem molestias omnis expedita libero repellendus quia suscipit voluptatum nihil, quos enim doloremque maiores quo, at dolore.</td>
+                    <td className='delete'><DeleteIcon/></td>
+                </tr><tr>
+                    <td>Duncan</td>
+                    <td>2</td>
+                    <td>254712345678</td>
+                    <td>Family House</td>
+                    <td>1 bdrm</td>
+                    <td>Nairobi</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum blanditiis architecto error omnis. Deleniti omnis iste culpa sunt odio reiciendis.</td>
+                    <td className='delete'><DeleteIcon/></td>
+                </tr><tr>
+                    <td>Joseph</td>
+                    <td>12</td>
+                    <td>254712345678</td>
+                    <td>Apartment</td>
+                    <td>3 bdrm</td>
+                    <td>Ruiru</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium error natus debitis enim praesentium dolore sint vel ducimus expedita minima?</td>
+                    <td className='delete'><DeleteIcon/></td>
+                </tr>
+                <tr>
+                    <td>Michael</td>
+                    <td>12</td>
+                    <td>254712345678</td>
+                    <td>Apartment</td>
+                    <td>2 bdrm</td>
+                    <td>Nairobi</td>
+                    <td>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, asperiores.</td>
+                    <td className='delete'><DeleteIcon/></td>
+                </tr>
+                </table>
     </div>
   );
 }
