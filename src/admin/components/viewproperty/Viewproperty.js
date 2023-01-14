2f85d93512f805c6d32abd0d3adb87d1805cd86d
Ashley <ashleynjeri7@gmail.com>
2023-01-14 15:26:26 +0300

    Added handleSave function

--- src/admin/components/viewproperty/Viewproperty.js
+++ src/admin/components/viewproperty/Viewproperty.js
@@ -52,6 +52,13 @@ const Viewproperty = () => {
     updatedData[index][field] = e.target.value;
     setState({...state, data: updatedData});
   }
+
+  const handleSave = () => {
+    const confirmSave = window.confirm("Are you sure you want to save the changes?");
+    if (confirmSave) {
+      setState({...state, editingIndex: -1});
+    }
+  }
   
 
   return (
