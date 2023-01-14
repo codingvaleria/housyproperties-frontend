b94b92a3ac1eb048581d4b53d050f293c42ce0fd
Ashley <ashleynjeri7@gmail.com>
2023-01-14 22:38:34 +0300

    Added tenant data file

---
+++ src/admin/components/viewtenant/tenantData.js
@@ -0,0 +1,44 @@
+const tenantData = [
+    {
+        id: 1,
+        name: "John Doe",
+        hse_num: "1",
+        phone: "08012345678",
+        hse_type: "Apartment",
+        unit_type: "2 Bedroom",
+        date_in: "12/12/2020",
+        balance: "2,000,000",
+    },
+    {
+        id: 2,
+        name: "Jane Doe",
+        hse_num: "2",
+        phone: "08012345678",
+        hse_type: "Villa",
+        unit_type: "7 Bedroom",
+        date_in: "12/12/2020",
+        balance: "32,000,000",
+    },
+    {
+        id: 3,
+        name: "John Doe",
+        hse_num: "3",
+        phone: "08012345678",
+        hse_type: "Family House",
+        unit_type: "4 Bedroom",
+        date_in: "12/12/2020",
+        balance: "15,000,000",
+    },
+    {
+        id: 4,
+        name: "John Doe",
+        hse_num: "4",
+        phone: "08012345678",
+        hse_type: "Apartment",
+        unit_type: "2 Bedroom",
+        date_in: "12/12/2020",
+        balance: "2,000,000",
+    }
+]
+
+export default tenantData;
