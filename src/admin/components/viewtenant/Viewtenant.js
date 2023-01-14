934ea542ae4946717b3e5980a2cf3717a0bc5e89
Ashley <ashleynjeri7@gmail.com>
2023-01-14 22:25:43 +0300

    Added table headers

--- src/admin/components/viewtenant/Viewtenant.js
+++ src/admin/components/viewtenant/Viewtenant.js
@@ -7,5 +7,18 @@ function Viewtenant() {
       <AdminNavBar />
       <div className="tenant-table-container">
           <div className="tenant-table-title">Tenants' Table</div>
+          <table className="tenant-table">
+            <thead className="tenant-table-header">
+              <tr>
+                <th className="column-name">Name</th>
+                <th className="column-hse-num">House No.</th>
+                <th className="column-phone">Phone No.</th>
+                <th className="column-hse-type">House Type</th>
+                <th className="column-unit">Unit Type</th>
+                <th className="column-date">Date In</th>
+                <th className="column-balance">Balance</th>
+                <th className="column-action">Action</th>
+              </tr>
+            </thead>
 
 export default Viewtenant;
