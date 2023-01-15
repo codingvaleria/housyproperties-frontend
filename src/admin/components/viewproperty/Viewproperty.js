baa0955f5c2bc6a7c96e16f6d1467a7b885a8d81
Ashley Njoroge <53012069+AshleyN2@users.noreply.github.com>
2023-01-15 21:13:53 +0300

    Add View Property Edits (#30)

--- src/admin/components/viewproperty/Viewproperty.js
+++ src/admin/components/viewproperty/Viewproperty.js
@@ -3,24 +3,77 @@ import { useState } from "react";
 import  logo from "../../../images/logo.png"
 import "./Viewproperty.css"
 import propertiesData from "./propertiesData";
+// import React, { useState, useEffect } from 'react'; FOR API CONSUMPTION
 import AdminBanner from "../adminhome/AdminBanner";
 import AdminNavBar from "../adminhome/AdminNavbar";
 
 const Viewproperty = () => {
-  const [data, setData] = useState(propertiesData);
-
-  const handleDelete = (index) => {
-    const updatedData = [...data];
-    updatedData.splice(index, 1);
-    setData(updatedData);
+  const initialState = {
+    data: propertiesData,
+    editingIndex: -1,
+    newProperties: {
+      name: "",
+      type: "",
+      unit_type: "",
+      location: "",
+      amount: ""
+    }
   }
+  
+  const [state, setState] = useState(initialState);
+
+// FOR API CONSUMPTION
+  // const Viewproperty = () => {
+  //   const [state, setState] = useState({
+  //     data: [],
+  //     editingIndex: -1,
+  //     newProperties: {
+  //       name: "",
+  //       type: "",
+  //       unit_type: "",
+  //       location: "",
+  //       amount: ""
+  //     }
+  //   });
+  
+  //   useEffect(() => {
+  //     fetch('https://your-api-endpoint.com/properties')
+  //       .then(response => response.json())
+  //       .then(data => setState({ ...state, data }))
+  //       .catch(error => console.error(error));
+  //   }, []);
 
   const handleEdit = (index) => {
-    const updatedData = [...data];
-    updatedData[index].name = "Edited";
-    // make changes to the specific item in updatedData 
-    setData(updatedData);
-}
+    setState({...state, editingIndex: index});
+  }
+
+  const handleChange = (e, index, field) => {
+    const updatedData = [...state.data];
+    updatedData[index][field] = e.target.value;
+    setState({...state, data: updatedData});
+  }
+
+  const handleSave = () => {
+    const confirmSave = window.confirm("Are you sure you want to save the changes?");
+    if (confirmSave) {
+      setState({...state, editingIndex: -1});
+    }
+  }
+
+  const handleCancel = () => {
+    setState({...state, editingIndex: -1});
+  }
+
+  const handleDelete = (index) => {
+    const confirmDelete = window.confirm("Are you sure you want to delete this property?");
+    if (confirmDelete) {
+      const updatedData = [...state.data];
+      updatedData.splice(index, 1);
+      setState({...state, data: updatedData});
+    }
+  }
+
+  
   return (
     <div>
       <AdminNavBar />
@@ -39,27 +92,58 @@ const Viewproperty = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => (
+        {state.data.map((row, index) => (
             <tr key={index} className="property-table-row">
-              <td>{row.name}</td>
-              <td>
-                <img className="p-img" src={logo} alt={row.name} width="100" />
-              </td>
-              <td>{row.type}</td>
-              <td>{row.unit_type}</td>
-              <td>{row.location}</td>
-              <td>{row.amount}</td>
-              <td>
-                <button className="p-edit-button" onClick={() => handleEdit(index)}>Edit</button>
-                <button className="p-delete-button" onClick={() => handleDelete(index)}>Delete</button>
-              </td>
-              </tr>
+              {state.editingIndex === index ?
+             (
+              <>
+                  <td>
+                    <input value={row.name} onChange={e => handleChange(e, index, "name")} />
+                  </td>
+                  <td>
+                    <img className="p-img" src={logo} alt={row.name} width="100" />
+                  </td>
+                  <td>
+                    <input value={row.type} onChange={e => handleChange(e, index, "type")} />
+                  </td>
+                  <td>
+                    <input value={row.unit_type} onChange={e => handleChange(e, index, "unit_type")} />
+                  </td>
+                  <td>
+                    <input value={row.location} onChange={e => handleChange(e, index, "location")} />
+                  </td>
+                  <td>
+                    <input value={row.amount} onChange={e => handleChange(e, index, "amount")} />
+                  </td>
+                  <td>
+                    <button className="p-edit-button" onClick={() => handleSave(index)}>Save</button>
+                    <button className="p-delete-button" onClick={() => handleCancel(index)}>Cancel</button>
+                  </td>
+                </>
+              ) : (
+                <>
+                  <td>{row.name}</td>
+                  <td>
+                    <img className="p-img" src={logo} alt={row.name} width="100" />
+                  </td>
+                  <td>{row.type}</td>
+                  <td>{row.unit_type}</td>
+                  <td>{row.location}</td>
+                  <td>{row.amount}</td>
+                  <td>
+                    <button className="p-edit-button" onClick={() => handleEdit(index)}>Edit</button>
+                    <button className="p-delete-button" onClick={() => handleDelete(index)}>Delete</button>
+                  </td>
+                </>
+              )}
+            </tr>
           ))}
         </tbody>
       </table>
-    </div>
+      </div>
     </div>
   );
-}
+};
+
 
 export default Viewproperty;
