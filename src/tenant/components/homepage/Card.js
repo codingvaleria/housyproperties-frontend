60096657ed2b8045bc8086d21dc3173cd67701a7
codingvaleria <codingvaleria@gmail.com>
2023-01-16 17:38:51 +0300

    Refactor housecards and cards

--- src/tenant/components/homepage/Card.js
+++ src/tenant/components/homepage/Card.js
@@ -2,33 +2,35 @@ import React from "react";
 import "./Card.css";
 
 function Card({
+  propertyname,
+  propertytype,
+  unittype,
+  location,
   image,
-  estateName,
-  title,
   network,
   shower,
-  parking,
   balcony,
-  price,
+  parking,
+  amount,
 }) {
   return (
     <div className="card">
       {/* set background image for styling images with unstandard size */}
       <div className="hse-image" style={{ backgroundImage: `url(${image})` }} />
       <div className="cardInfo">
-        <h6 className="estate">{estateName}</h6>
-        <h4>{title}</h4>
+        <h6 className="estate">{propertyname}</h6>
+        <h4>{`${unittype} ${propertytype} ${location}`}</h4>
         <div className="infoSections">
           <div className="section1">
             <div className="infoicons">
               <h6>
-                <i class="symbol fa fa-wifi" aria-hidden="true"></i>
+                <i className="symbol fa fa-wifi" aria-hidden="true"></i>
                 {network}
               </h6>
             </div>
             <div className="infoicons">
               <h6>
-                <i class=" symbol fa fa-shower" aria-hidden="true"></i>
+                <i className=" symbol fa fa-shower" aria-hidden="true"></i>
                 {shower}
               </h6>
             </div>
@@ -36,17 +38,17 @@ function Card({
           <div className="section2">
             <div className="infoicons">
               <h6>
-                <i class=" symbol fa fa-car" aria-hidden="true"></i>
+                <i className=" symbol fa fa-car" aria-hidden="true"></i>
                 {parking}
               </h6>
             </div>
             <div className="infoicons">
-              <i class="symbol fa fa-bath" aria-hidden="true"></i>
+              <i className="symbol fa fa-bath" aria-hidden="true"></i>
               <h6>{balcony}</h6>
             </div>
           </div>
         </div>
-        <button className="price-button">{price}</button>
+        <button className="price-button">{amount}</button>
       </div>
     </div>
   );
--- src/tenant/components/homepage/Housecards.js
+++ src/tenant/components/homepage/Housecards.js
@@ -1,71 +1,31 @@
 import React from "react";
 import Card from "./Card";
+import data from "./data";
 
 function Housecards() {
+  let properties = data.map((e) => {
+    return (
+      <Card
+        key={e.id}
+        propertyname={e.propertyname}
+        propertytype={e.propertytype}
+        unittype={e.unittype}
+        location={e.location}
+        image={e.image}
+        network={e.network}
+        shower={e.shower}
+        balcony={e.balcony}
+        parking={e.parking}
+        amount={e.amount}
+      />
+    );
+  });
+
+  console.log(properties);
+
   return (
     <div className="houses">
-      <div className="cards-container">
-        <Card
-          image="https://i.pinimg.com/564x/eb/23/16/eb2316a4c199cb12436f6b9f440a2330.jpg"
-          estateName="Kagundo"
-          title=" 2 Bedrooms Villa Rongai"
-          network="Wifi"
-          shower="Shower"
-          parking="Parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/0f/8b/a5/0f8ba5b8d78ed7c43ababd190aafb282.jpg"
-          estateName="Oak"
-          title="1 Bedroom Villa Kimbo"
-          network="Wifi"
-          shower="Shower"
-          parking="Parking"
-          balcony="Balcony"
-          price="Ksh 30,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/01/6e/ab/016eaba835b4ae2b4743c8c145586f93.jpg"
-          estateName="Rock Beach"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/5d/94/90/5d94908bebfc5fed86b87eaf7e969ad1.jpg"
-          estateName="Kagundo"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/5d/94/90/5d94908bebfc5fed86b87eaf7e969ad1.jpg"
-          estateName="Kagundo"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/07/99/cf/0799cfc4db021ced811249df3e45f936.jpg"
-          estateName="Kagundo"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 30,000"
-        />
-      </div>
+      <div className="cards-container">{properties}</div>
     </div>
   );
 }
