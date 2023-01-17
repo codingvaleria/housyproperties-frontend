cf25f941b31737d38aa80ca66a235ee1ec56e424
codingvaleria <codingvaleria@gmail.com>
2023-01-17 06:07:54 +0300

    Fetch from backend

--- src/tenant/components/homepage/Card.js
+++ src/tenant/components/homepage/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 
 function Card({
-  propertyname,
+  propertyName,
   propertytype,
   unittype,
   location,
@@ -14,43 +14,44 @@ function Card({
   amount,
 }) {
   return (
-    <div className="card">
-      {/* set background image for styling images with unstandard size */}
-      <div className="hse-image" style={{ backgroundImage: `url(${image})` }} />
-      <div className="cardInfo">
-        <h6 className="estate">{propertyname}</h6>
-        <h4>{`${unittype} ${propertytype} ${location}`}</h4>
-        <div className="infoSections">
-          <div className="section1">
-            <div className="infoicons">
-              <h6>
-                <i className="symbol fa fa-wifi" aria-hidden="true"></i>
-                {network}
-              </h6>
-            </div>
-            <div className="infoicons">
-              <h6>
-                <i className=" symbol fa fa-shower" aria-hidden="true"></i>
-                {shower}
-              </h6>
-            </div>
-          </div>
-          <div className="section2">
-            <div className="infoicons">
-              <h6>
-                <i className=" symbol fa fa-car" aria-hidden="true"></i>
-                {parking}
-              </h6>
-            </div>
-            <div className="infoicons">
-              <i className="symbol fa fa-bath" aria-hidden="true"></i>
-              <h6>{balcony}</h6>
-            </div>
-          </div>
-        </div>
-        <button className="price-button">{amount}</button>
-      </div>
-    </div>
+    <div> </div>
+    // <div className="card">
+    //   {/* set background image for styling images with unstandard size */}
+    //   <div className="hse-image" style={{ backgroundImage: `url(${image})` }} />
+    //   <div className="cardInfo">
+    //     <h6 className="estate">{propertyName}</h6>
+    //     <h4>{`${unittype} ${propertytype} ${location}`}</h4>
+    //     <div className="infoSections">
+    //       <div className="section1">
+    //         <div className="infoicons">
+    //           <h6>
+    //             <i className="symbol fa fa-wifi" aria-hidden="true"></i>
+    //             {network}
+    //           </h6>
+    //         </div>
+    //         <div className="infoicons">
+    //           <h6>
+    //             <i className=" symbol fa fa-shower" aria-hidden="true"></i>
+    //             {shower}
+    //           </h6>
+    //         </div>
+    //       </div>
+    //       <div className="section2">
+    //         <div className="infoicons">
+    //           <h6>
+    //             <i className=" symbol fa fa-car" aria-hidden="true"></i>
+    //             {parking}
+    //           </h6>
+    //         </div>
+    //         <div className="infoicons">
+    //           <i className="symbol fa fa-bath" aria-hidden="true"></i>
+    //           <h6>{balcony}</h6>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <button className="price-button">{amount}</button>
+    //   </div>
+    // </div>
   );
 }
 
--- src/tenant/components/homepage/Housecards.js
+++ src/tenant/components/homepage/Housecards.js
@@ -1,27 +1,122 @@
 import React from "react";
-import Card from "./Card";
-
+// import data from "./data";
+import { useState, useEffect } from "react";
+import "./Card.css";
 
 function Housecards() {
+  let [data, setData] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let fetchData = async () => {
+      setIsLoading(true);
+      try {
+        let response = await fetch(
+          "https://housy-properties-production.up.railway.app/properties"
+        );
+        let data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // console.log(properties, data);
+
+  if (isLoading) {
+    <div className="houses">
+      <p>Loading ....</p>
+    </div>;
+  }
+
+  if (!data) {
+    return (
+      <div className="houses">
+        <p>No data</p>
+      </div>
+    );
+  }
+
+  function handleShower(b) {
+    if (b === true) {
+      return "Shower";
+    } else {
+      return "No Shower";
+    }
+  }
+
+  function handleWifi(b) {
+    if (b === true) {
+      return "Wifi";
+    } else {
+      return "No Wifi";
+    }
+  }
+
+  function handleParking(b) {
+    if (b === true) {
+      return "Parking";
+    } else {
+      return "No Parking";
+    }
+  }
+
+  function handleBalcony(b) {
+    if (b === true) {
+      return "Balcony";
+    } else {
+      return "No Balcony";
+    }
+  }
+
   let properties = data.map((e) => {
     return (
-      <Card
-        key={e.id}
-        propertyname={e.propertyname}
-        propertytype={e.propertytype}
-        unittype={e.unittype}
-        location={e.location}
-        image={e.image}
-        network={e.network}
-        shower={e.shower}
-        balcony={e.balcony}
-        parking={e.parking}
-        amount={e.amount}
-      />
+      <div className="card">
+        {/* set background image for styling images with unstandard size */}
+        <div
+          className="hse-image"
+          style={{ backgroundImage: `url(${e.image})` }}
+        />
+        <div className="cardInfo">
+          <h6 className="estate">{e.propertyName}</h6>
+          <h4>{`${e.unit_type} ${e.property_type} ${e.location}`}</h4>
+          <div className="infoSections">
+            <div className="section1">
+              <div className="infoicons">
+                <h6>
+                  <i className="symbol fa fa-wifi" aria-hidden="true"></i>
+                  {handleWifi(e.wifi)}
+                </h6>
+              </div>
+              <div className="infoicons">
+                <h6>
+                  <i className=" symbol fa fa-shower" aria-hidden="true"></i>
+                  {handleShower(e.shower)}
+                </h6>
+              </div>
+            </div>
+            <div className="section2">
+              <div className="infoicons">
+                <h6>
+                  <i className=" symbol fa fa-car" aria-hidden="true"></i>
+                  {handleParking(e.parking)}
+                </h6>
+              </div>
+              <div className="infoicons">
+                <i className="symbol fa fa-bath" aria-hidden="true"></i>
+                <h6>{handleBalcony(e.balcony)}</h6>
+              </div>
+            </div>
+          </div>
+          <button className="price-button">{e.amount}</button>
+        </div>
+      </div>
     );
   });
-
-  console.log(properties);
 
   return (
     <div className="houses">
