f0a8d54c565976d527dead6f0e86a241e188a8bc
codingvaleria <106528361+codingvaleria@users.noreply.github.com>
2023-01-17 12:42:05 +0300

    Fn/housecards  (#41)

--- src/tenant/components/homepage/Housecards.js
+++ src/tenant/components/homepage/Housecards.js
@@ -1,71 +1,98 @@
 import React from "react";
+// import data from "./data";
+import { useState, useEffect } from "react";
+import "./Card.css";
 import Card from "./Card";
 
 function Housecards() {
+  let [data, setData] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let fetchData = async () => {
+      setIsLoading(true);
+      try {
+        let response = await fetch(
+          "https://housy-properties-production.up.railway.app/properties"
+        );
+        let data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    <div className="houses">
+      <p>Loading ....</p>
+    </div>;
+  }
+
+  if (!data) {
+    return (
+      <div className="houses">
+        <p>No data</p>
+      </div>
+    );
+  }
+
+  function handleShower(b) {
+    if (b === true) {
+      return "Shower";
+    } else {
+      return "No Shower";
+    }
+  }
+
+  function handleWifi(b) {
+    if (b === true) {
+      return "Wifi";
+    } else {
+      return "No Wifi";
+    }
+  }
+
+  function handleParking(b) {
+    if (b === true) {
+      return "Parking";
+    } else {
+      return "No Parking";
+    }
+  }
+
+  function handleBalcony(b) {
+    if (b === true) {
+      return "Balcony";
+    } else {
+      return "No Balcony";
+    }
+  }
+
+  let properties = data.map((e) => {
+    return (
+      <Card
+        key={e.id}
+        propertyName={e.propertyName}
+        property_type={e.property_type}
+        unit_type={e.unit_type}
+        location={e.location}
+        image={e.image}
+        wifi={handleWifi(e.wifi)}
+        shower={handleShower(e.shower)}
+        balcony={handleBalcony(e.balcony)}
+        parking={handleParking(e.parking)}
+        amount={e.amount}
+      />
+    );
+  });
+
   return (
     <div className="houses">
-      <div className="cards-container">
-        <Card
-          image="https://i.pinimg.com/564x/eb/23/16/eb2316a4c199cb12436f6b9f440a2330.jpg"
-          estateName="Kagundo"
-          title=" 2 Bedrooms Villa Rongai"
-          network="Wifi"
-          shower="Shower"
-          parking="Parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/0f/8b/a5/0f8ba5b8d78ed7c43ababd190aafb282.jpg"
-          estateName="Oak"
-          title="1 Bedroom Villa Kimbo"
-          network="Wifi"
-          shower="Shower"
-          parking="Parking"
-          balcony="Balcony"
-          price="Ksh 30,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/01/6e/ab/016eaba835b4ae2b4743c8c145586f93.jpg"
-          estateName="Rock Beach"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/5d/94/90/5d94908bebfc5fed86b87eaf7e969ad1.jpg"
-          estateName="Kagundo"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/5d/94/90/5d94908bebfc5fed86b87eaf7e969ad1.jpg"
-          estateName="Kagundo"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 20,000"
-        />
-        <Card
-          image="https://i.pinimg.com/564x/07/99/cf/0799cfc4db021ced811249df3e45f936.jpg"
-          estateName="Kagundo"
-          title="2 Bedrooms Villa Karen"
-          network="Wifi"
-          shower="Shower"
-          parking="parking"
-          balcony="Balcony"
-          price="Ksh 30,000"
-        />
-      </div>
+      <div className="cards-container">{properties}</div>
     </div>
   );
 }
