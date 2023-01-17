ed3c9b31e772a98ccab119c64cb7e5bd8f18d9a2
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2023-01-17 21:10:00 +0300

    add navigation to footer

--- src/tenant/components/footer/Footer.js
+++ src/tenant/components/footer/Footer.js
@@ -29,17 +29,27 @@ function Footer() {
                   Home
                 </li>
               </a>
-              <a href="">
-                <li> Property Types</li>
+              <a href="#property_type">
+                <li
+                 onClick={() => {
+                  nav("/");
+                }}> Property Types</li>
+              </a>
+              <a href="#properties">
+                <li
+                 onClick={() => {
+                  nav("/");
+                }}
+                > Become a Tenant</li>
+              </a>
+              <a href="#contact">
+                <li onClick={() => {
+                    nav("/about");
+                  }}> Contact Us</li>
               </a>
               <a href="">
-                <li> Become a Tenant</li>
-              </a>
-              <a href="">
-                <li> Contact Us</li>
-              </a>
-              <a href="">
-                <li> Back To Top</li>
+                <li
+                > Back To Top</li>
               </a>
             </ul>
           </div>
@@ -65,8 +75,13 @@ function Footer() {
                   About Us
                 </li>
               </a>
-              <a href="">
-                <li> Our Properties</li>
+              <a href="#properties">
+                <li
+                 onClick={() => {
+                  nav("/");
+                }}
+                > Our Properties
+                </li>
               </a>
               <a href="">
                 <li
