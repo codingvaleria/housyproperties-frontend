4c1b2b84f700da8ce96e2937b924f71697e93d2a
Benson <benson.odongo@student.moringaschool.com>
2023-01-18 08:27:23 +0300

    add property functionality

--- src/admin/components/addproperty/Addpropertyform.js
+++ src/admin/components/addproperty/Addpropertyform.js
@@ -3,17 +3,17 @@ import AdminNavBar from "../adminhome/AdminNavbar";
 import "./Addpropertyform.css";
 import { useNavigate } from "react-router-dom";
 
-function Addpropertyform( onAddProperty ) {
-  const [ propertyName, setPropertyName] = useState("")
-  const [property_type, setProperty_type] = useState("")
-  const [unit_type, setUnit_type] = useState("")
-const [location, setLocation] = useState("")
-const [image, setImage] = useState(" ")
-const [amount, setAmount] = useState(" ")
-const [wifi, setWifi] = useState("")
-const [shower, setShower] = useState("")
-const [balcony, setBalcony] = useState("")
-const [parking, setParking] = useState("")
+function Addpropertyform(onAddProperty) {
+  const [propertyName, setPropertyName] = useState("");
+  const [propertyType, setProperty_type] = useState("");
+  const [unitType, setUnit_type] = useState("");
+  const [location, setLocation] = useState("");
+  const [image, setImage] = useState(" ");
+  const [amount, setAmount] = useState(" ");
+  const [wifi, setWifi] = useState("");
+  const [shower, setShower] = useState("");
+  const [balcony, setBalcony] = useState("");
+  const [parking, setParking] = useState("");
   const formData = {
     propertyName: "",
     property_type: "",
@@ -22,29 +22,41 @@ const [parking, setParking] = useState("")
     image: "",
     amount: "",
     wifi: true,
-    shower:true,
+    shower: true,
     balcony: true,
-    parking: true
+    parking: true,
   };
 
   const navigate = useNavigate();
 
   function handleSubmit(e) {
-    console.log(formData);
+    const property = {
+      propertyName: propertyName,
+      property_type: propertyType,
+      unit_type: unitType,
+      location: location,
+      image: image,
+      amount: amount,
+      wifi: true,
+      shower: true,
+      balcony: true,
+      parking: true,
+    };
+
+    console.log(property);
     e.preventDefault();
     fetch("https://housy-properties-production.up.railway.app/properties", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData),
-    })
-    .then((resp)=>{
+      body: JSON.stringify(property),
+    }).then((resp) => {
       if (resp.ok) {
         alert("propertyhas been successfully added");
-        e.reset()
+        e.reset();
       }
-    })
+    });
   }
 
   return (
@@ -62,39 +74,36 @@ const [parking, setParking] = useState("")
                 placeholder="Property Name"
                 id="Property Name"
                 name="Property Name"
-                onChange={(e)=>setPropertyName(e.target.value)}
+                onChange={(e) => setPropertyName(e.target.value)}
                 value={propertyName}
-
               />
             </div>
             <div className="input-field">
               <div className="custom-select">
                 <i className="icon fa fa-home" aria-hidden="true"></i>
                 <input
-                className="form-control"
-                type="text"
-                placeholder="Property Type"
-                id="Location"
-                name="Location"
-                onChange={(e)=>setProperty_type(e.target.value)}
-                value={property_type}
-
-              />
+                  className="form-control"
+                  type="text"
+                  placeholder="Property Type"
+                  id="Location"
+                  name="Location"
+                  onChange={(e) => setProperty_type(e.target.value)}
+                  value={propertyType}
+                />
               </div>
             </div>
             <div className="input-field">
               <div className="custom-select">
                 <i className="icon fa fa-home" aria-hidden="true"></i>
                 <input
-                className="form-control"
-                type="text"
-                placeholder="Unit Type"
-                id="Location"
-                name="Location"
-                onChange={(e)=>setUnit_type(e.target.value)}
-                value={unit_type}
-
-              />
+                  className="form-control"
+                  type="text"
+                  placeholder="Unit Type"
+                  id="Location"
+                  name="Location"
+                  onChange={(e) => setUnit_type(e.target.value)}
+                  value={unitType}
+                />
               </div>
             </div>
             <div className="input-field">
@@ -105,9 +114,8 @@ const [parking, setParking] = useState("")
                 placeholder="Location"
                 id="Location"
                 name="Location"
-                onChange={(e)=>setLocation(e.target.value)}
+                onChange={(e) => setLocation(e.target.value)}
                 value={location}
-
               />
             </div>
             <div className="input-field">
@@ -118,9 +126,8 @@ const [parking, setParking] = useState("")
                 placeholder="Image"
                 id="Image"
                 name="Image"
-                onChange={(e)=>setImage(e.target.value)}
+                onChange={(e) => setImage(e.target.value)}
                 value={image}
-
               />
             </div>
             <div className="input-field">
@@ -131,9 +138,8 @@ const [parking, setParking] = useState("")
                 placeholder="Amount"
                 id="Amount"
                 name="Amount"
-                onChange={(e)=>setAmount(e.target.value)}
+                onChange={(e) => setAmount(e.target.value)}
                 value={amount}
-
               />
             </div>
             <div className="facilities-wrapper">
@@ -145,9 +151,8 @@ const [parking, setParking] = useState("")
                   type="checkbox"
                   id="Wifi"
                   name="Wifi"
-                  onChange={(e)=>setWifi (e.target.value)}
-                value={wifi}
-
+                  onChange={(e) => setWifi(e.target.value)}
+                  value={wifi}
                 />
               </div>
               <div className="check-field">
@@ -157,9 +162,8 @@ const [parking, setParking] = useState("")
                   type="checkbox"
                   id="Shower"
                   name="Shower"
-                  onChange={(e)=>setShower(e.target.value)}
-                value={shower}
-
+                  onChange={(e) => setShower(e.target.value)}
+                  value={shower}
                 />
               </div>
               <div className="check-field">
@@ -169,9 +173,8 @@ const [parking, setParking] = useState("")
                   type="checkbox"
                   id="Balcony"
                   name="Balcony"
-                  onChange={(e)=>setBalcony(e.target.value)}
-                value={balcony}
-
+                  onChange={(e) => setBalcony(e.target.value)}
+                  value={balcony}
                 />
               </div>
               <div className="check-field">
@@ -181,15 +184,18 @@ const [parking, setParking] = useState("")
                   type="checkbox"
                   id="Parking"
                   name="Parking"
-                  onChange={(e)=>setParking(e.target.value)}
-                value={parking}
-
+                  onChange={(e) => setParking(e.target.value)}
+                  value={parking}
                 />
               </div>
             </div>
 
             <div className="submit-wrapper">
-              <input type="submit" value="Add Property" onSubmit={handleSubmit}></input>
+              <input
+                type="submit"
+                value="Add Property"
+                onSubmit={handleSubmit}
+              ></input>
             </div>
           </div>
         </form>
